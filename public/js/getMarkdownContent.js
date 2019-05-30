6225850875f7e9401903473176d287b9aa0b75b5
Queenie <flora.huot@epitech.eu>
2019-05-30 18:53:10 +0100

    Changed a bit of css

--- public/js/getMarkdownContent.js
+++ public/js/getMarkdownContent.js
@@ -12,31 +12,31 @@ right.appendChild(containerRight);
 getMarkdownContent();
 
 function getMarkdownContent(url = "https://raw.githubusercontent.com/raven-os/getting-started/master/README.md") {
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.onload = function () {
-        if (request.status >= 200 && request.status < 400) {
-            // markdown to html
-            container.innerHTML = marked(this.response);
-            // remove old information in right sideebar
-            while (containerRight.hasChildNodes()) {
-                containerRight.removeChild(containerRight.lastChild);
-            }
-            // update information in right sidebar
-            const listH2 = document.getElementsByTagName("h2");
-            for (var i = 0; i < listH2.length; i++) {
-                const li = document.createElement('li');
-                const a = document.createElement('a')
-                li.appendChild(a);
-                containerRight.appendChild(li);
-                a.href = '#' + listH2[i].id;
-                a.innerHTML = listH2[i].innerHTML
-            }
-        } else {
-            const errorMessage = document.createElement('p');
-            errorMessage.textContent = `Failed to load!`;
-            app.appendChild(errorMessage);
-        }
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      // markdown to html
+      container.innerHTML = marked(this.response);
+      // remove old information in right sideebar
+      while (containerRight.hasChildNodes()) {
+        containerRight.removeChild(containerRight.lastChild);
+      }
+      // update information in right sidebar
+      const listH2 = document.getElementsByTagName("h2");
+      for (var i = 0; i < listH2.length; i++) {
+        const li = document.createElement('li');
+        const a = document.createElement('a')
+        li.appendChild(a);
+        containerRight.appendChild(li);
+        a.href = '#' + listH2[i].id;
+        a.innerHTML = listH2[i].innerHTML
+      }
+    } else {
+      const errorMessage = document.createElement('p');
+      errorMessage.textContent = `Failed to load!`;
+      app.appendChild(errorMessage);
     }
-    request.send();
+  }
+  request.send();
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-(function(document, history, location) {
+(function (document, history, location) {
   var HISTORY_SUPPORT = !!(history && history.pushState);
 
   var anchorScrolls = {
@@ -8,7 +8,7 @@
     /**
      * Establish events, and fix initial scroll position if a hash is provided.
      */
-    init: function() {
+    init: function () {
       this.scrollToCurrent();
       $(window).on('hashchange', $.proxy(this, 'scrollToCurrent'));
       $('body').on('click', 'a', $.proxy(this, 'delegateAnchors'));
@@ -18,7 +18,7 @@
      * Return the offset amount to deduct from the normal scroll position.
      * Modify as appropriate to allow for dynamic calculations
      */
-    getFixedOffset: function() {
+    getFixedOffset: function () {
       return this.OFFSET_HEIGHT_PX;
     },
 
@@ -28,48 +28,48 @@
      * @param  {String} href
      * @return {Boolean} - Was the href an anchor.
      */
-    scrollIfAnchor: function(href, pushToHistory) {
+    scrollIfAnchor: function (href, pushToHistory) {
       var match, anchorOffset;
 
-      if(!this.ANCHOR_REGEX.test(href)) {
+      if (!this.ANCHOR_REGEX.test(href)) {
         return false;
       }
 
       match = document.getElementById(href.slice(1));
 
-      if(match) {
+      if (match) {
         anchorOffset = $(match).offset().top - this.getFixedOffset();
-        $('html, body').animate({ scrollTop: anchorOffset});
+        $('html, body').animate({ scrollTop: anchorOffset });
 
         // Add the state to history as-per normal anchor links
-        if(HISTORY_SUPPORT && pushToHistory) {
+        if (HISTORY_SUPPORT && pushToHistory) {
           history.pushState({}, document.title, location.pathname + href);
         }
       }
 
       return !!match;
     },
-    
+
     /**
      * Attempt to scroll to the current location's hash.
      */
-    scrollToCurrent: function(e) { 
-      if(this.scrollIfAnchor(window.location.hash) && e) {
-      	e.preventDefault();
+    scrollToCurrent: function (e) {
+      if (this.scrollIfAnchor(window.location.hash) && e) {
+        e.preventDefault();
       }
     },
 
     /**
      * If the click event's target was an anchor, fix the scroll position.
      */
-    delegateAnchors: function(e) {
+    delegateAnchors: function (e) {
       var elem = e.target;
 
-      if(this.scrollIfAnchor(elem.getAttribute('href'), true)) {
+      if (this.scrollIfAnchor(elem.getAttribute('href'), true)) {
         e.preventDefault();
       }
     }
   };
 
-	$(document).ready($.proxy(anchorScrolls, 'init'));
+  $(document).ready($.proxy(anchorScrolls, 'init'));
 })(window.document, window.history, window.location);
